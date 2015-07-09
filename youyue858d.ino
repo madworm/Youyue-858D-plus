14a42cd45046958953cab74840c9e6b58edf039e
Moritz Augsburger <moritz@augsburger.name>
2015-07-09 13:47:44 +0200

    release 1.4.2
    - fan only can be toggled by pressing both buttons at once
    - "FAN" and acutal temperature are shown alternating while cooling down
    - temperature in set mode blinks slightly

--- youyue858d.ino
+++ youyue858d.ino
@@ -2,7 +2,7 @@
  * This is a custom firmware for my 'Youyue 858D+' hot-air soldering station.
  * It may or may not be useful to you, always double check if you use it.
  *
- * V1.41
+ * V1.42
  *
  * 2015 - Robert Spitzenpfeil
  * 2015 - Moritz Augsburger
@@ -98,7 +98,7 @@
 
 #define FW_MAJOR_V 1
 #define FW_MINOR_V_A 4
-#define FW_MINOR_V_B 1
+#define FW_MINOR_V_B 2
 /*
  * PC5: FAN-speed (A5 in Arduino lingo) (OK)
  * PC3: TIP122.base --> FAN (OK)
