e36fa974425e03ea3b643e1bf72617aa1891e480
Robert Spitzenpfeil <robert@linuxbox.site>
2015-07-12 12:03:34 +0200

    Revert "Fixed \n strangeness in 'release.sh'."
    This reverts commit 3d41058e10ae8787ba697e094175216bbc72e3b5.

--- release.sh
+++ release.sh
@@ -22,7 +22,8 @@ if [[ -z ${vmaj} || -z ${vmia} || -z ${vmib}  ]]; then
     exit 2
 fi
 
-vline=$(cut -d'\n' -f5 youyue858d.ino | sed "s/^ \\* ${vers}$//" )
+vline=$(cut -d'
+' -f5 youyue858d.ino | sed "s/^ \\* ${vers}$//" )
 
 echo "vline: '$vline'"
 
