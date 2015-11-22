madworm/Youyue-858D-plus

commit e9aa2a40193d1966fbe2e7b066db9554a5fbf236

Yet another comment regarding the BLDC FAN-speed mod.

youyue858d.h
@@ -131,14 +131,20 @@ uint8_t get_key_common_l(uint8_t key_mask);
 #define FAN_ON_TEMP 60
 #define FAN_OFF_TEMP_FANONLY (SAFE_TO_TOUCH_TEMP - 2)
 
-#define FAN_SPEED_MIN_DEFAULT 150UL
-#define FAN_SPEED_MAX_DEFAULT 360UL
+#define FAN_SPEED_MIN_DEFAULT 450UL
+#define FAN_SPEED_MAX_DEFAULT 800UL
 
 // 
 // good starting values with BLDC FAN-speed mod
 //
 // #define FAN_SPEED_MIN_DEFAULT 450UL
 // #define FAN_SPEED_MAX_DEFAULT 800UL
+//
+// don't forget to extend the ranges in the .ino
+//
+// CPARAM fan_speed_min = { 0, 999, FAN_SPEED_MIN_DEFAULT, FAN_SPEED_MIN_DEFAULT, 18, 19 };
+// CPARAM fan_speed_max = { 0, 999, FAN_SPEED_MAX_DEFAULT, FAN_SPEED_MAX_DEFAULT, 20, 21 };
+//
 
 #define FAN_CURRENT_MIN_DEFAULT 30UL
 #define FAN_CURRENT_MAX_DEFAULT 71UL

commit 72cb1fa4ec317d9898b7a70e9bfdea4ece9a176e

Fixed 'fan-speed-mod' related error introduced in e9aa2a40.

youyue858d.h
@@ -131,8 +131,8 @@ uint8_t get_key_common_l(uint8_t key_mask);
 #define FAN_ON_TEMP 60
 #define FAN_OFF_TEMP_FANONLY (SAFE_TO_TOUCH_TEMP - 2)
 
-#define FAN_SPEED_MIN_DEFAULT 450UL
-#define FAN_SPEED_MAX_DEFAULT 800UL
+#define FAN_SPEED_MIN_DEFAULT 150UL
+#define FAN_SPEED_MAX_DEFAULT 360UL
 
 // 
 // good starting values with BLDC FAN-speed mod